93758b60e955175f6f70881c4f5fa5c2e21dbffd
iHnKwuuN <6nMnueBijq>
2022-03-06 19:27:18 +0400

    reverse

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    message = request.GET['message']
+    return render(request, 'reverse.html', {'original_message': message, 'reversed_message': message[::-1]})
